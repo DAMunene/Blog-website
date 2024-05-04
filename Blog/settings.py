788be32c10ec4e69b45496809d5eed6bc00a5188
DAMunene <muriukimoff6@gmail.com>
2024-05-04 18:14:17 +0300

    created a profiles template and signals file

--- Blog/settings.py
+++ Blog/settings.py
@@ -122,8 +122,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL =  'login'
+
 
 
 
